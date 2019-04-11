17b6410b13f822b405d55d62217ce2556f2915fd
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-11 20:05:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === "") {
+  if (!cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
