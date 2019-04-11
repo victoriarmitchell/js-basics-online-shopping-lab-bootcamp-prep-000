dbf9c626ef38e89174d09148847acf44921b79e6
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-11 20:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    cart = []
+    cart = [];
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
